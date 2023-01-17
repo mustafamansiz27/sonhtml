342548e1f49943ceac58c77dea4f67e85554407b
mustafamansiz27@gmail.com <=>
2023-01-17 20:36:37 +0300

    last commit

---
+++ guess/index.js
@@ -0,0 +1,25 @@
+
+
+
+function start(){
+
+    
+const textAria = document.querySelector("#textAria");
+const btn1 = document.querySelector("#btn1")
+const btn2 = document.querySelector("#btn2")
+
+textAria.style.display="inline";
+btn1.style.display ="none";
+
+
+let num = textAria.value;
+
+if(!num || isNaN(num)) return ;
+
+
+}
+
+const randomNumber = (min,max ) =>{
+    let 
+}
+
--- readme.md
+++
@@ -1,2 +0,0 @@
-![https://patika.dev](https://global-uploads.webflow.com/6097e0eca1e87557da031fef/609859a191abe5d64b17fed3_Patika%20logo-p-500.png)
-[https://patika.dev](https://patika.dev)
